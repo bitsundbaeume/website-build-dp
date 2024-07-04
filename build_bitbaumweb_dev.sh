4b98f2c12bff03bce09f887e31a89299c90f4125
benjamin.kees <benjamin.kees@fiff.de>
2024-07-04 16:29:43 +0200

    logdiranpassungen

--- build_bitbaumweb_dev.sh
+++ build_bitbaumweb_dev.sh
@@ -8,15 +8,17 @@ USER=bitsundbaeume
 WEBROOTSDIR=/var/www/$USER/www
 REPO=/var/www/$USER/repositories/bubweb-$BRANCH
 LOCKDIR=/var/www/$USER/website_build_lock
+LOGDIR=/var/www/$USER/website_build_log
 
 CURRENT=$WEBROOTSDIR/bubweb-$BRANCH-current
 NEW=$WEBROOTSDIR/bubweb-$BRANCH-new
+
 LOGFILE=bitbaumweb_build_$BRANCH.log
 TIMEOUT=600 # in seconds
 PATIENCE=10 # seconds to wait before recheck if other build process has finished
 
 
-echo "$(date) $BRANCH Build Script triggered" | tee -a ~/logs/$LOGFILE
+echo "$(date) $BRANCH Build Script triggered" | tee -a $LOGDIR/$LOGFILE
 
 cd $REPO
 git fetch
@@ -27,9 +29,9 @@ counter=0
 while test -f $LOCKDIR/build_process_running.lock
 do
         timespent=$(expr $counter \* $PATIENCE)
-        echo "$(date) $BRANCH spent $timespent seconds waiting. Now waiting another $PATIENCE seconds for other BUILD to finish." | tee -a ~/logs/$LOGFILE
+        echo "$(date) $BRANCH spent $timespent seconds waiting. Now waiting another $PATIENCE seconds for other BUILD to finish." | tee -a $LOGDIR/$LOGFILE
         if [ "$timespent" -gt "$TIMEOUT" ]; then
-                echo "$(date) $BRANCH TIME OUT – GIVING UP." | tee -a ~/logs/$LOGFILE
+                echo "$(date) $BRANCH TIME OUT – GIVING UP." | tee -a $LOGDIR/$LOGFILE
                 exit
         fi
         sleep $PATIENCE
@@ -44,32 +46,32 @@ echo "$(date) $BRANCH build process is running" > $LOCKDIR/build_process_running
 echo "RUNNING npm install"
 echo "==================="
 echo ""
-echo -n "$(date) $BRANCH Running npm install..." | tee -a ~/logs/$LOGFILE
+echo -n "$(date) $BRANCH Running npm install..." | tee -a $LOGDIR/$LOGFILE
 if /usr/bin/npm install; then
-        echo "succeeded" | tee -a ~/logs/$LOGFILE
+        echo "succeeded" | tee -a $LOGDIR/$LOGFILE
 else
-        echo "failed" | tee -a ~/logs/$LOGFILE
+        echo "failed" | tee -a $LOGDIR/$LOGFILE
 fi
 
 echo "RUNNING npm run prepare"
 echo "======================="
 echo ""
-echo -n "$(date) $BRANCH Running npm run prepare ..." | tee -a ~/logs/$LOGFILE
+echo -n "$(date) $BRANCH Running npm run prepare ..." | tee -a $LOGDIR/$LOGFILE
 if /usr/bin/npm run prepare; then
-        echo "succeeded" | tee -a ~/logs/$LOGFILE
+        echo "succeeded" | tee -a $LOGDIR/$LOGFILE
 else
-        echo "failed" | tee -a ~/logs/$LOGFILE
+        echo "failed" | tee -a $LOGDIR/$LOGFILE
 fi
 
 
 echo "RUNNING npm build"
 echo "================="
 echo ""
-echo -n "$(date) $BRANCH Running npm run build ..." | tee -a ~/logs/$LOGFILE
+echo -n "$(date) $BRANCH Running npm run build ..." | tee -a $LOGDIR/$LOGFILE
 if /usr/bin/npm run build; then
-        echo "succeeded" | tee -a ~/logs/$LOGFILE
+        echo "succeeded" | tee -a $LOGDIR/$LOGFILE
 else
-        echo "failed" | tee -a ~/logs/$LOGFILE
+        echo "failed" | tee -a $LOGDIR/$LOGFILE
 fi
 
 if test -f $REPO/_site/index.html; then
@@ -84,15 +86,15 @@ if test -f $REPO/_site/index.html; then
 	ln -s $CURRENT $DOMAIN
 	
 	if test -f $DOMAIN/index.html; then
-		echo "$(date) $BRANCH – Update was published (index.html exists)." | tee -a ~/logs/$LOGFILE
+		echo "$(date) $BRANCH – Update was published (index.html exists)." | tee -a $LOGDIR/$LOGFILE
 	else
-		echo "$(date) $BRANCH – ERROR index.html not in $DOMAIN ." | tee -a ~/logs/$LOGFILE
+		echo "$(date) $BRANCH – ERROR index.html not in $DOMAIN ." | tee -a $LOGDIR/$LOGFILE
 	fi
 
 else
-	echo "$(date) $BRANCH – UPDATE WAS NOT PUBLISHED BECAUSE index.html DIDN'T EXIST." | tee -a ~/logs/$LOGFILE
+	echo "$(date) $BRANCH – UPDATE WAS NOT PUBLISHED BECAUSE index.html DIDN'T EXIST." | tee -a $LOGDIR/$LOGFILE
 fi
 
-echo "$(date) $BRANCH Build Script ended." | tee -a ~/logs/$LOGFILE
+echo "$(date) $BRANCH Build Script ended." | tee -a $LOGDIR/$LOGFILE
 
 rm $LOCKDIR/build_process_running.lock
